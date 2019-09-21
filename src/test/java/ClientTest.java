eb8128ae260835f844974f41434629d92ddac97f
Marcizz <46536014+Marcizz@users.noreply.github.com>
2019-09-21 13:05:56 +0200

    Lagt till @author

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,12 +1,16 @@
 import org.junit.Test;
 
+/***
+ * 
+ * @author Marcus Laitala (Marcizz) & Emil Albrektsson (Spirotris) 2019-09-21
+ *
+ */
 public class ClientTest {
-	
-	
+		
 	@Test
 	public void testConnectionToServer() {
 		// TODO: IMPLEMENT!
 	}
 	
-	
+		
 }
