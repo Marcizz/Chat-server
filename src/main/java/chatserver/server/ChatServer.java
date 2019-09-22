03c4f9fad73debfd2420dd17ac8d8ac7f207fa80
spirotris <38663830+spirotris@users.noreply.github.com>
2019-09-22 13:11:12 +0200

    Multi-client functionality implemented
    Messages between clients-test not yet green.

--- src/main/java/chatserver/server/ChatServer.java
+++ src/main/java/chatserver/server/ChatServer.java
@@ -1,42 +1,54 @@
 package chatserver.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ChatServer {
-
-	private ServerSocket server;
+public class ChatServer implements Runnable {
+	protected List<String> users = new ArrayList<String>();
+	protected List<HandleClient> clients = new ArrayList<HandleClient>();
+	ServerSocket server;
+	private int port;
 
 	public ChatServer(int port) {
-		try {
-			System.out.println("Waiting for client on port " + port);
-			server = new ServerSocket(port);
+		this.port = port;
+	}
 
-			Socket socket = server.accept();
-			
-			InputStream input = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-            OutputStream output = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(output, true);
-
-            writer.println(reader.readLine());
-            
-			server.close();
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
+	private void process(int port) throws Exception {
+		server = new ServerSocket(port);
+		System.out.println("Server Started...");
+		while (true) {
+			Socket client = server.accept();
+			HandleClient newUser = new HandleClient(client, this);
+			clients.add(newUser);
 		}
 	}
 
-	public ServerSocket getSocket() {
-		return server;
+	protected void broadcast(String user, String message) {
+		for (HandleClient client : clients) {
+			client.sendMessage(user, message);
+		}
+	}
+
+	public void run() {
+		try {
+			process(port);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			stopServer();
+		}
+
+	}
+
+	public void stopServer() {
+		try {
+			server.close();
+		} catch (IOException e) {
+			System.err.println("Error attempting to close server: " + e.getLocalizedMessage());
+		}
 	}
 
 }
--- src/test/java/chatserver/ServerTest.java
+++ src/test/java/chatserver/ServerTest.java
@@ -1,81 +1,101 @@
 package chatserver;
 
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.io.BufferedReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import chatserver.server.ChatServer;
 
 public class ServerTest {
 
-	ChatServer server;
+	private static ChatServer server;
 	String testServerIp = "127.0.0.1";
-	int testServerPort = 27500;
+	private static int testServerPort = 27510;
 
-	@Before
-	public void setupServer() {
-
-		new Thread(
-
-				new Runnable() {
-
-					public void run() {
-						server = new ChatServer(testServerPort);
-					}
-
-				}
-
-		).start();
-
+	@BeforeClass
+	public static void startServer() {
+		server = new ChatServer(testServerPort);
+		new Thread(server).start();
+	}
+	
+	@AfterClass
+	public static void stopServer() {
+		server.stopServer();
 	}
 
 	@Test(timeout = 2000)
 	public void testServerAcceptsConnections() {
 		try {
 			Socket client = new Socket(testServerIp, testServerPort);
-			assertTrue(client.isConnected());
+			assertFalse(client.isClosed());
 			client.close();
 		} catch (UnknownHostException e) {
-			e.printStackTrace();
+			fail(e.getLocalizedMessage());
 		} catch (IOException e) {
-			e.printStackTrace();
+			fail(e.getLocalizedMessage());
 		}
 	}
 
-	@Test(timeout = 2000)
+	@Test(timeout = 5000)
 	public void testServerReturnsMessagesToClient() {
 		String testMsg = "abcdef";
 		try {
 			Socket client = new Socket(testServerIp, testServerPort);
 
-			// Setup input/output stream
 			OutputStream out = client.getOutputStream();
 			PrintWriter writer = new PrintWriter(out, true);
 
 			InputStream in = client.getInputStream();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
-			// Send message
 			writer.println(testMsg);
 			String response = reader.readLine();
-			
+
 			assertTrue(testMsg.contentEquals(response));
 			client.close();
 		} catch (Exception e) {
-			e.printStackTrace();
+			fail(e.getLocalizedMessage());
 		}
 	}
 
+	@Test(timeout = 2000)
+	public void testServerSendsMessageBetweenClients() {
+		String testMsg = "message from first client";
+		try {
+			Socket client1 = new Socket(testServerIp, testServerPort);
+			Socket client2 = new Socket(testServerIp, testServerPort);
+
+			// Setup streams: client1 send, client2 receive
+			OutputStream out = client1.getOutputStream();
+			PrintWriter writer = new PrintWriter(out, true);
+
+			InputStream in = client2.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+			// Send message to server with client1
+			writer.println(testMsg);
+
+			// Check response from client2
+			String response = reader.readLine();
+
+			assertTrue(testMsg.contentEquals(response));
+			client1.close();
+			client2.close();
+		} catch (Exception e) {
+			fail(e.getLocalizedMessage());
+		}
+	}
 }
